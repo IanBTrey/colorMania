007a55b8eca865f6b91126e26165d6e4eb7376af
IanBTrey <kibetkirui010@gmail.com>
2019-02-19 18:28:26 +0300

    add changes

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+        $("button#green").click(function() {
+          $("body").removeClass();
+          $("body").addClass("green-background");
+        });
+
+        $("button#purple").click(function() {
+          $("body").removeClass();
+          $("body").addClass("purple-background");
+        });
+
+        $("button#red").click(function() {
+          $("body").removeClass();
+          $("body").addClass("red-background");
+        });
+      });
